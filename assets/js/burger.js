d5eceff8c206f68c75d41ea85346ffb1851d6d0e
Milo Bonnet <milovtt@gmail.com>
2022-12-16 15:18:37 +0100

    Menu burger

---
+++ assets/js/burger.js
@@ -0,0 +1,8 @@
+    let buttom = document.querySelector('#menu')
+    buttom.addEventListener('click', function (){
+    
+        let menu = document.querySelector('.header-nav');
+
+        menu.classList.toggle('menumobile');
+
+    })
